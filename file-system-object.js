3a9d66b829c369795d6628adf2b9558ddee07179
David Stone <davidjamesstone@gmail.com>
2016-09-25 20:12:59 +0100

    Update

--- file-system-object.js
+++ file-system-object.js
@@ -6,17 +6,9 @@ var FileSystemObject = function (path, stat) {
   this.path = path
   this.dir = p.dirname(path)
   this.isDirectory = typeof stat === 'boolean' ? stat : stat.isDirectory()
+  this.isFile = !this.isDirectory
   this.ext = p.extname(path)
   this.stat = stat
   this.filesize = filesize(this.stat.size, { bits: true, round: 0 })
-  // var relativeDir = p.relative(process.cwd(), this.dir)
-  // this.relativeDir = relativeDir
-  // var relativePath = p.relative(process.cwd(), this.path)
-  // this.relativePath = relativePath
-}
-FileSystemObject.prototype = {
-  get isFile () {
-    return !this.isDirectory
-  }
 }
 module.exports = FileSystemObject
